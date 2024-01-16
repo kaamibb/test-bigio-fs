72999fc6bcfbff292447bb38d3279bef96b45506
Bima Arya Kurniawan <120927463+kaamibb@users.noreply.github.com>
2024-01-17 03:08:14 +0700

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://test-bigio-fs.vercel.app/posts";
+const url = "https://test-bigio-fs.vercel.app/";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
